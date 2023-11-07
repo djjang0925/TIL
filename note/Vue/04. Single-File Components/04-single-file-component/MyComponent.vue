fc2f83083ae90412799b459b4a02d8331dafcda0
뚱이 <djjang0925@gmail.com>
2023-11-07 11:16:55 +0900

    231107

---
+++ note/Vue/04. Single-File Components/04-single-file-component/MyComponent.vue
@@ -0,0 +1,17 @@
+<!-- vbase-3-setup -->
+<template>
+  <div class="greeting">{{ msg }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('hello world!')
+</script>
+
+<!-- scss문법을 사용하지 않으므로 지울 것 -->
+<style scoped>
+.greeting {
+  color: crimson;
+}
+</style>
