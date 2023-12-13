a2bca64f617ed0943c7573e9f47572bde2593728
뚱이 <djjang0925@gmail.com>
2023-12-14 01:21:08 +0900

    231214

--- React/06. Small Projects/movie/src/App.js
+++ React/06. Small Projects/movie/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/movie:id`} element={<Detail />}></Route>
+        <Route path={`${process.env.PUBLIC_URL}/hello`} element={<h1>Hello</h1>}></Route>
       </Routes>
     </Router>
   );
